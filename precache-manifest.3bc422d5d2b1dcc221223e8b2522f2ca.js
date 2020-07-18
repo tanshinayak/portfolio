58fa0a13907cbcdc1a7f8a77c361d52770227a38
Tanshi Nayak <tanshitn@gmail.com>
2020-07-18 17:45:15 +0530

    Updates

---
+++ precache-manifest.3bc422d5d2b1dcc221223e8b2522f2ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb5e5e2369c0bbc24a78acff87599dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a6b188062415026f85fc",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3d9df9f3ccfcb06aa9de",
+    "url": "/portfolio/static/css/main.c489adf3.chunk.css"
+  },
+  {
+    "revision": "a6b188062415026f85fc",
+    "url": "/portfolio/static/js/2.0d0869ba.chunk.js"
+  },
+  {
+    "revision": "3d9df9f3ccfcb06aa9de",
+    "url": "/portfolio/static/js/main.fe0433ce.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "31b02b3cb942266dc78f0ba90158b74d",
+    "url": "/portfolio/static/media/background.31b02b3c.jpg"
+  },
+  {
+    "revision": "7cb4e3a6b32319046a496cd9ad823516",
+    "url": "/portfolio/static/media/gif.7cb4e3a6.jpg"
+  },
+  {
+    "revision": "766471f5b33e5441daa0d4ab7d66367e",
+    "url": "/portfolio/static/media/insta.766471f5.png"
+  },
+  {
+    "revision": "2e1f46e65c67510393f1e8c597b433de",
+    "url": "/portfolio/static/media/profile.2e1f46e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tanshinayak.github.io/precache-manifest.89f66ed0ae7386f4643c328dcc3a4e19.js"
+  "/portfolio/precache-manifest.3bc422d5d2b1dcc221223e8b2522f2ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tanshinayak.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
